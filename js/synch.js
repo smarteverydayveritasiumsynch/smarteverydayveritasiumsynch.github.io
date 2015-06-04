6fbbeeb95dcbf3d027bf3c154b94a8f099e41e31
smarteverydayveritasiumsynch <alebedev@2ch.hk>
2015-06-04 19:17:27 +0300

    Update synch.js

--- js/synch.js
+++ js/synch.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+	//low quality playback
 	$("#play-videos").click(function(){
 		playVideos('aDorTBEhEtk' , 'ihv4f7VMeJw');
 	});
@@ -8,13 +10,23 @@ $(document).ready(function() {
 		$(".videos").html('<iframe src="https://www.youtube.com/embed/'+url1+'?autoplay=1&loop=1&rel=0&wmode=transparent" frameborder="0"></iframe><iframe src="https://www.youtube.com/embed/'+url2+'?autoplay=1&loop=1&rel=0&wmode=transparent" frameborder="0"></iframe>');
 	};
 
+	//720p playback
+	$("#play-videoshd720").click(function(){
+		playVideosHD720('aDorTBEhEtk' , 'ihv4f7VMeJw');
+	});
+
+	function playVideosHD720(url1, url2){
+		"use strict";
+		$(".videos").html('<object><param name="movie" value="https://www.youtube.com/v/'+url1+'?autoplay=1&loop=1&rel=0&vq=hd720"></param><param name="wmode" value="transparent"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="https://www.youtube.com/v/'+url1+'?autoplay=1&loop=1&rel=0&vq=hd720" type="application/x-shockwave-flash" allowscriptaccess="always" wmode="transparent" allowfullscreen="true"></embed></object><object><param name="movie" value="https://www.youtube.com/v/'+url2+'?autoplay=1&loop=1&rel=0&vq=hd720&wmode=transparent"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="https://www.youtube.com/v/'+url2+'?autoplay=1&loop=1&rel=0&vq=hd720&wmode=transparent" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true"></embed></object>');
+	};
+
+	//1080p playback
 	$("#play-videoshd").click(function(){
 		playVideosHD('aDorTBEhEtk' , 'ihv4f7VMeJw');
 	});
 
 	function playVideosHD(url1, url2){
 		"use strict";
-		$(".videos").html('<object><param name="movie" value="https://www.youtube.com/v/'+url1+'?autoplay=1&loop=1&rel=0&vq=hd1080&"></param><param name="wmode" value="transparent"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="https://www.youtube.com/v/'+url1+'?autoplay=1&loop=1&rel=0&vq=hd1080" type="application/x-shockwave-flash" allowscriptaccess="always" wmode="transparent" allowfullscreen="true"></embed></object><object><param name="movie" value="https://www.youtube.com/v/'+url2+'?autoplay=1&loop=1&rel=0&vq=hd1080&wmode=transparent"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="https://www.youtube.com/v/'+url2+'?autoplay=1&loop=1&rel=0&vq=hd1080&wmode=transparent" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true"></embed></object>');
+		$(".videos").html('<object><param name="movie" value="https://www.youtube.com/v/'+url1+'?autoplay=1&loop=1&rel=0&vq=hd1080"></param><param name="wmode" value="transparent"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="https://www.youtube.com/v/'+url1+'?autoplay=1&loop=1&rel=0&vq=hd1080" type="application/x-shockwave-flash" allowscriptaccess="always" wmode="transparent" allowfullscreen="true"></embed></object><object><param name="movie" value="https://www.youtube.com/v/'+url2+'?autoplay=1&loop=1&rel=0&vq=hd1080&wmode=transparent"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="https://www.youtube.com/v/'+url2+'?autoplay=1&loop=1&rel=0&vq=hd1080&wmode=transparent" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true"></embed></object>');
 	};
-
-});
+s});
